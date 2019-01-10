61c4264932febe66ee7acdfa8360c360dc60c12e
Parveen <praveengarg3786@gmail.com>
2019-01-10 11:42:57 +0530

    arrayList program

---
+++ java1/Array_list.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Array_List
+{
+	public static void main(String arg[])
+		{
+			ArrayList<String> ls=new ArrayList<String>();
+			ls.add("apple");
+			ls.add("mango");
+			ls.add("grapes");
+			System.out.println(ls);
+		}
+
+}
